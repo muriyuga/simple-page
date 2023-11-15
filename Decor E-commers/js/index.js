65f84538e3653a2276483f1de03bfc7320b9b58b
Muriyuga <muriyugadesuga@gmail.com>
2023-11-15 14:51:52 +0700

    Ini Versi Lebih Baik

--- Decor E-commers/js/index.js
+++ Decor E-commers/js/index.js
@@ -1,22 +1,12 @@
-const datas = [
-  { name: "Collection", id: 1 },
-  { name: "Shop", id: 2 },
-  { name: "About", id: 3 },
-  { name: "Wishlits", id: 4 },
-  { name: "Login", id: 5 },
-  { name: "Bag", id: 6 }
-]
+function menu() {
+  const navItem = document.querySelector('.nav-item');
+  navItem.classList.toggle('active');  
+};
 
-let lection = document.getElementById("colection").innerHTML = datas[0].name
-let shop = document.getElementById("shop").innerHTML = datas[1].name
-let about = document.getElementById("about").innerHTML = datas[2].name
-let wishl = document.getElementById("wishlist").innerHTML = datas[3].name
-let login = document.getElementById("login").innerHTML = datas[4].name
-let bag = document.getElementById("bag").innerHTML = datas[5].name;
-
-const menuToggle = document.querySelector('.menu-toggle span')
-const nav = document.querySelector('.navbar .nav-item')
-
-menuToggle.addEventListener('click', function() {
-  nav.classList.toggle('menu')
-})
+document.addEventListener('click', function(event) {
+  const hamberger = document.querySelector('#humberger-menu');
+  const navItems = document.querySelector('.nav-item');
+  if(!hamberger.contains(event.target) && !navItems.contains(event.target)) {
+    navItems.classList.remove('active');
+  };
+});
